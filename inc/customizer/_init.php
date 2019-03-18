d43f9b5b69f79a16cb238b2f7f76efeeb17d9cd2
Kyle Jennings <kylejennings@kyles-mbp.lan>
2019-03-18 08:29:05 -0400

    started adding WPCS to the customizer files

--- inc/customizer/_init.php
+++ inc/customizer/_init.php
@@ -1,27 +1,25 @@
 <?php
 
 $files = array(
-    '_helpers.php',
-    '_sanitizations.php',
-
-    'controls/label.php',
-    'controls/sortable.php',
-    'controls/color-scheme.php',
-    'controls/menu-dropdown.php',
-    'controls/checkbox-group.php',
-    
-    'identity.php',
-    'header.php',
-    'template-settings.php',
-    'frontpage.php',
-    'widgetized.php',
-    'footer.php',
-    'page-404.php',
+	'_helpers.php',
+	'_sanitizations.php',
+	'controls/label.php',
+	'controls/sortable.php',
+	'controls/color-scheme.php',
+	'controls/menu-dropdown.php',
+	'controls/checkbox-group.php',
+	'identity.php',
+	'header.php',
+	'template-settings.php',
+	'frontpage.php',
+	'widgetized.php',
+	'footer.php',
+	'page-404.php',
 );
 
 // load all the settings files
-foreach ($files as $file) {
-    require_once $file;
+foreach ( $files as $file ) {
+	require_once $file;
 }
 
 
@@ -30,19 +28,18 @@ foreach ($files as $file) {
  * enqueues scripts to the WordPress Customizer
  * @return [type] [description]
  */
-function benjamin_customizer_enqueue()
-{
+function benjamin_customizer_enqueue() {
 
-  // this script is minified, however a non minified version is included with the theme
-    wp_enqueue_script(
-        'custom-customize',
-        get_template_directory_uri() . '/assets/admin/js/_benjamin-customizer-min.js',
-        null,
-        '20170215',
-        true
-    );
+	// this script is minified, however a non minified version is included with the theme.
+	wp_enqueue_script(
+		'custom-customize',
+		get_template_directory_uri() . '/assets/admin/js/_benjamin-customizer-min.js',
+		null,
+		'20170215',
+		true
+	);
 }
-add_action('customize_controls_enqueue_scripts', 'benjamin_customizer_enqueue');
+add_action( 'customize_controls_enqueue_scripts', 'benjamin_customizer_enqueue' );
 
 
 
@@ -50,18 +47,17 @@ add_action('customize_controls_enqueue_scripts', 'benjamin_customizer_enqueue');
  * enqueues scripts to the WordPress Previewer
  * @return [type] [description]
  */
-function benjamin_previewer_enqueue()
-{
-    wp_enqueue_script(
-        'custom-customize',
-        get_template_directory_uri() . '/assets/frontend/js/_benjamin-previewer-min.js',
-        null,
-        '20170215',
-        true
-    );
+function benjamin_previewer_enqueue() {
+	wp_enqueue_script(
+		'custom-customize',
+		get_template_directory_uri() . '/assets/frontend/js/_benjamin-previewer-min.js',
+		null,
+		'20170215',
+		true
+	);
 }
 
-add_action('customize_preview_init', 'benjamin_previewer_enqueue');
+add_action( 'customize_preview_init', 'benjamin_previewer_enqueue' );
 
 
 
@@ -70,44 +66,41 @@ add_action('customize_preview_init', 'benjamin_previewer_enqueue');
  * Active Callbacks for 5.2 support ::shakes fist::
  * ----------------------------------------------------------------------------
  */
+function benjamin_active_callback_filter( $active, $control ) {
+	global $wp_customize;
 
-function benjamin_active_callback_filter($active, $control)
-{
-    global $wp_customize;
+	$toggled_by = isset( $control->input_attrs['data-toggled-by'] ) ? $control->input_attrs['data-toggled-by'] : null;
 
-    $toggled_by = isset($control->input_attrs['data-toggled-by']) ? $control->input_attrs['data-toggled-by'] : null;
+	if ( strpos( $toggled_by, '_settings_active' ) && $toggled_by !== BENJAMIN_DEFAULT_TEMPLATE . '_settings_active' ) {
+		return 'yes' === $wp_customize->get_setting( $toggled_by )->value();
+	} elseif ( $control->id == '_404_header_page_content_control' ) {
+		return 'page' == $wp_customize->get_setting( '_404_hero_content_setting' )->value();
+	} elseif ( $control->id == '_404_page_select_control' ) {
+		return 'page' == $wp_customize->get_setting( '_404_page_content_setting' )->value();
+	} elseif ( $control->id == 'frontpage_hero_callout_control' ) {
+		return 'callout' === $wp_customize->get_setting( 'frontpage_hero_content_setting' )->value();
+	} elseif ( $control->id == 'frontpage_hero_page_control' ) {
+		return 'page' == $wp_customize->get_setting( 'frontpage_hero_content_setting' )->value();
+	} elseif ( $control->id == 'banner_read_more_control' || $control->id == 'banner_text_control' ) {
+		return 'display' == $wp_customize->get_setting( 'banner_visibility_setting' )->value();
+	} elseif ( strpos( $toggled_by, '_sidebar_position_setting' ) ) {
 
-    if (strpos($toggled_by, '_settings_active') && $toggled_by !== BENJAMIN_DEFAULT_TEMPLATE . '_settings_active') {
-        return 'yes' === $wp_customize->get_setting($toggled_by)->value();
-    } elseif ($control->id == '_404_header_page_content_control') {
-        return 'page' == $wp_customize->get_setting('_404_hero_content_setting')->value();
-    } elseif ($control->id == '_404_page_select_control') {
-        return 'page' == $wp_customize->get_setting('_404_page_content_setting')->value();
-    } elseif ($control->id == 'frontpage_hero_callout_control') {
-        return 'callout' === $wp_customize->get_setting('frontpage_hero_content_setting')->value();
-    } elseif ($control->id == 'frontpage_hero_page_control') {
-        return 'page' == $wp_customize->get_setting('frontpage_hero_content_setting')->value();
-    } elseif ($control->id == 'banner_read_more_control' || $control->id == 'banner_text_control') {
-        return 'display' == $wp_customize->get_setting('banner_visibility_setting')->value();
-    } elseif (strpos($toggled_by, '_sidebar_position_setting')) {
+		$pos             = strpos( $toggled_by, '_sidebar_position_setting' );
+		$prefix          = substr( $toggled_by, 0, $pos );
+		$pos             = 'none' !== $wp_customize->get_setting( $toggled_by )->value();
+		$settings_active = $prefix . '_settings_active';
 
-        $pos = strpos($toggled_by, '_sidebar_position_setting');
-        $prefix = substr($toggled_by, 0, $pos);
-        $pos = 'none' !== $wp_customize->get_setting($toggled_by)->value();
-        $settings_active = $prefix . '_settings_active';
+		if ( $prefix == 'default' ) {
+			return $pos;
+		}
 
-        if ($prefix == 'default') {
-            return $pos;
-        }
+		$section = 'yes' === $wp_customize->get_setting( $settings_active )->value();
 
-        $section = 'yes' === $wp_customize->get_setting($settings_active)->value();
+		return $pos === $section ? true : false;
+	}
 
-        return $pos == $section ? true : false;
-    }
-
-    return $active;
-
+	return $active;
 
 }
 
-add_filter('customize_control_active', 'benjamin_active_callback_filter', 100, 2);
+add_filter( 'customize_control_active', 'benjamin_active_callback_filter', 100, 2 );
--- inc/customizer/header.php
+++ inc/customizer/header.php
@@ -9,232 +9,258 @@
  * @param  [type] $wp_customize [description]
  * @return [type]               [description]
  */
-function benjamin_header_settings($wp_customize)
-{
+function benjamin_header_settings( $wp_customize ) {
 
-    $banner_label = __('Banner', 'benjamin');
-    $navbar_label = __('Navbar', 'benjamin');
-    $hero_label = __('Hero', 'benjamin');
-    $default_json = '[';
-    $default_json .= '{"name":"banner","label": "' . $banner_label . '"}, ';
-    $default_json .= '{"name":"navbar","label":"' . $navbar_label . '"}, ';
-    $default_json .= '{"name":"hero","label":"' . $hero_label . '"}';
-    $default_json .= ']';
+	$banner_label  = __( 'Banner', 'benjamin' );
+	$navbar_label  = __( 'Navbar', 'benjamin' );
+	$hero_label    = __( 'Hero', 'benjamin' );
+	$default_json  = '[';
+	$default_json .= '{"name":"banner","label": "' . $banner_label . '"}, ';
+	$default_json .= '{"name":"navbar","label":"' . $navbar_label . '"}, ';
+	$default_json .= '{"name":"hero","label":"' . $hero_label . '"}';
+	$default_json .= ']';
 
-    // the section
-    $wp_customize->add_section(
-        'header_settings_section',
-        array(
-            'title'          => __('Header Settings', 'benjamin'),
-            'priority'       => 30,
-        )
-    );
+	// the section.
+	$wp_customize->add_section(
+		'header_settings_section',
+		array(
+			'title'    => __( 'Header Settings', 'benjamin' ),
+			'priority' => 30,
+		)
+	);
 
-    $wp_customize->add_setting(
-        'header_sortables_setting',
-        array(
-            'default' => $default_json,
-            'sanitize_callback' => 'benjamin_header_sortable_sanitize',
-        )
-    );
+	$wp_customize->add_setting(
+		'header_sortables_setting',
+		array(
+			'default'           => $default_json,
+			'sanitize_callback' => 'benjamin_header_sortable_sanitize',
+		)
+	);
 
-    // header size
-    $header_components = array(
-        'navbar' => __('Navbar', 'benjamin'),
-        'hero' => __('Hero', 'benjamin'),
-        'banner' => __('Banner', 'benjamin')
-    );
+	// header size.
+	$header_components = array(
+		'navbar' => __( 'Navbar', 'benjamin' ),
+		'hero'   => __( 'Hero', 'benjamin' ),
+		'banner' => __( 'Banner', 'benjamin' ),
+	);
 
+	$description = __(
+		'The header area is made of sortable parts.  Simply drag these
+		parts around to change the order they are displayed.',
+		'benjamin'
+	);
 
-    $description = __('The header area is made of sortable parts.  Simply drag these
-        parts around to change the order they are displayed.', 'benjamin');
+	$wp_customize->add_control(
+		new Benjamin_Sortable_Control(
+			$wp_customize,
+			'header_sortables_control',
+			array(
+				'description' => sprintf( '%s', $description ),
+				'label'       => __( 'Header Order', 'benjamin' ),
+				'section'     => 'header_settings_section',
+				'settings'    => 'header_sortables_setting',
+				'choices'     => $header_components,
+			)
+		)
+	);
 
-    $wp_customize->add_control(
-        new Benjamin_Sortable_Control($wp_customize,
-            'header_sortables_control',
-            array(
-                'description' => sprintf('%s', $description),
-                'label' => __('Header Order', 'benjamin'),
-                'section' => 'header_settings_section',
-                'settings' => 'header_sortables_setting',
-                'choices' => $header_components
-            )
-        )
-    );
+	/**
+	 * Label
+	 */
+	$wp_customize->add_setting(
+		'navbar_label',
+		array(
+			'default'           => 'none',
+			'sanitize_callback' => 'wp_filter_nohtml_kses',
+		)
+	);
 
+	$wp_customize->add_control(
+		new Benjamin_Label_Custom_Control(
+			$wp_customize,
+			'navbar_label_control',
+			array(
+				'label'    => __( 'Navbar Settings', 'benjamin' ),
+				'type'     => 'label',
+				'section'  => 'header_settings_section',
+				'settings' => 'navbar_label',
+			)
+		)
+	);
 
-    /**
-     * Label
-     */
-    $wp_customize->add_setting(
-        'navbar_label', array(
-            'default' => 'none',
-            'sanitize_callback' => 'wp_filter_nohtml_kses',
-        )
-    );
+	$wp_customize->add_setting(
+		'navbar_search_setting',
+		array(
+			'default'           => 'none',
+			'sanitize_callback' => 'benjamin_navbar_search_setting_sanitize',
+		)
+	);
 
+	$wp_customize->add_control(
+		'navbar_search_control',
+		array(
+			'label'    => __( 'Display Search in Navbar', 'benjamin' ),
+			'section'  => 'header_settings_section',
+			'settings' => 'navbar_search_setting',
+			'type'     => 'select',
+			'choices'  => array(
+				'none'   => 'No',
+				'navbar' => 'Yes',
+			),
+		)
+	);
 
-    $wp_customize->add_control(
-        new Benjamin_Label_Custom_Control(
-            $wp_customize,
-            'navbar_label_control',
-            array(
-                'label' => __('Navbar Settings', 'benjamin'),
-                'type' => 'label',
-                'section' => 'header_settings_section',
-                'settings' => 'navbar_label',
-            )
-        )
-    );
+	$wp_customize->add_setting(
+		'navbar_color_setting',
+		array(
+			'default'           => 'light',
+			'sanitize_callback' => 'benjamin_navbar_color_setting_sanitize',
+		)
+	);
 
+	$wp_customize->add_control(
+		'navbar_color_control',
+		array(
+			'label'    => __( 'Navbar Color Scheme', 'benjamin' ),
+			'section'  => 'header_settings_section',
+			'settings' => 'navbar_color_setting',
+			'type'     => 'select',
+			'choices'  => array(
+				'light' => 'Light',
+				'dark'  => 'Dark',
+			),
+		)
+	);
 
-    $wp_customize->add_setting('navbar_search_setting', array(
-        'default' => 'none',
-        'sanitize_callback' => 'benjamin_navbar_search_setting_sanitize',
-    ) );
+	$wp_customize->add_setting(
+		'navbar_sticky_setting',
+		array(
+			'default'           => 'no',
+			'sanitize_callback' => 'benjamin_navbar_sticky_sanitize',
+		)
+	);
 
-    $wp_customize->add_control('navbar_search_control', array(
-            'label' => __('Display Search in Navbar', 'benjamin'),
-            'section' => 'header_settings_section',
-            'settings' => 'navbar_search_setting',
-            'type' => 'select',
-            'choices' => array(
-                'none' => 'No',
-                'navbar' => 'Yes',
-            )
-        )
-    );
+	$wp_customize->add_control(
+		'navbar_sticky_control',
+		array(
+			'description' => __( 'Stick the navbar to the top of the screen when you scroll down the page', 'benjamin' ),
+			'label'       => __( 'Navbar sticky on scroll', 'benjamin' ),
+			'section'     => 'header_settings_section',
+			'settings'    => 'navbar_sticky_setting',
+			'type'        => 'select',
+			'choices'     => array(
+				'no'  => 'No',
+				'yes' => 'Yes',
+			),
+		)
+	);
 
+	$wp_customize->add_setting(
+		'navbar_brand_setting',
+		array(
+			'default'           => 'text',
+			'sanitize_callback' => 'benjamin_navbar_brand_sanitize',
+		)
+	);
 
+	$wp_customize->add_control(
+		'navbar_brand_control',
+		array(
+			'description' => 'Display your site logo, or site name in the navbar',
+			'label'       => __( 'Navbar Brand Type', 'benjamin' ),
+			'section'     => 'header_settings_section',
+			'settings'    => 'navbar_brand_setting',
+			'type'        => 'select',
+			'choices'     => array(
+				'text' => 'Text',
+				'logo' => 'Logo',
+			),
+		)
+	);
 
+	/**
+	 * Label
+	 */
+	$wp_customize->add_setting(
+		'banner_label',
+		array(
+			'default'           => 'none',
+			'sanitize_callback' => 'wp_filter_nohtml_kses',
+		)
+	);
 
-    $wp_customize->add_setting('navbar_color_setting', array(
-        'default' => 'light',
-        'sanitize_callback' => 'benjamin_navbar_color_setting_sanitize',
-    ) );
+	$wp_customize->add_control(
+		new Benjamin_Label_Custom_Control(
+			$wp_customize,
+			'banner_label_control',
+			array(
+				'label'    => __( 'Banner Settings', 'benjamin' ),
+				'type'     => 'label',
+				'section'  => 'header_settings_section',
+				'settings' => 'banner_label',
+			)
+		)
+	);
 
-    $wp_customize->add_control('navbar_color_control', array(
-            'label' => __('Navbar Color Scheme', 'benjamin'),
-            'section' => 'header_settings_section',
-            'settings' => 'navbar_color_setting',
-            'type' => 'select',
-            'choices' => array(
-                'light' => 'Light',
-                'dark' => 'Dark',
-            )
-        )
-    );
+	$wp_customize->add_setting(
+		'banner_visibility_setting',
+		array(
+			'default'           => 'hide',
+			'sanitize_callback' => 'benjamin_banner_visibility_sanitize',
+		)
+	);
 
+	$wp_customize->add_control(
+		'banner_visibility_control',
+		array(
+			'description' => 'Display your site banner',
+			'label'       => __( 'Banner Visibility', 'benjamin' ),
+			'section'     => 'header_settings_section',
+			'settings'    => 'banner_visibility_setting',
+			'type'        => 'select',
+			'choices'     => array(
+				'hide'    => 'Hide',
+				'display' => 'Display',
+			),
+		)
+	);
 
-    $wp_customize->add_setting('navbar_sticky_setting', array(
-        'default' => 'no',
-        'sanitize_callback' => 'benjamin_navbar_sticky_sanitize',
-    ) );
+	$wp_customize->add_setting(
+		'banner_text_setting',
+		array(
+			'default'           => '',
+			'sanitize_callback' => 'wp_filter_nohtml_kses',
+		)
+	);
 
-    $wp_customize->add_control('navbar_sticky_control', array(
-            'description' => __('Stick the navbar to the top of the screen when you scroll down the page', 'benjamin'),
-            'label' => __('Navbar sticky on scroll', 'benjamin'),
-            'section' => 'header_settings_section',
-            'settings' => 'navbar_sticky_setting',
-            'type' => 'select',
-            'choices' => array(
-                'no' => 'No',
-                'yes' => 'Yes',
-            )
-        )
-    );
+	$wp_customize->add_control(
+		'banner_text_control',
+		array(
+			'label'    => __( 'Banner Text', 'benjamin' ),
+			'section'  => 'header_settings_section',
+			'settings' => 'banner_text_setting',
+			'type'     => 'text',
+		)
+	);
 
-    $wp_customize->add_setting('navbar_brand_setting', array(
-        'default' => 'text',
-        'sanitize_callback' => 'benjamin_navbar_brand_sanitize',
-    ) );
+	$wp_customize->add_setting(
+		'banner_read_more_setting',
+		array(
+			'default'           => '',
+			'sanitize_callback' => 'wp_filter_nohtml_kses',
+		)
+	);
 
-    $wp_customize->add_control('navbar_brand_control', array(
-            'description' => 'Display your site logo, or site name in the navbar',
-            'label' => __('Navbar Brand Type', 'benjamin'),
-            'section' => 'header_settings_section',
-            'settings' => 'navbar_brand_setting',
-            'type' => 'select',
-            'choices' => array(
-                'text' => 'Text',
-                'logo' => 'Logo',
-            )
-        )
-    );
-
-
-
-    /**
-     * Label
-     */
-    $wp_customize->add_setting(
-        'banner_label', array(
-            'default' => 'none',
-            'sanitize_callback' => 'wp_filter_nohtml_kses',
-        )
-    );
-
-
-    $wp_customize->add_control(
-        new Benjamin_Label_Custom_Control(
-            $wp_customize,
-            'banner_label_control',
-            array(
-                'label' => __('Banner Settings', 'benjamin'),
-                'type' => 'label',
-                'section' => 'header_settings_section',
-                'settings' => 'banner_label',
-            )
-        )
-    );
-
-
-    $wp_customize->add_setting('banner_visibility_setting', array(
-        'default' => 'hide',
-        'sanitize_callback' => 'benjamin_banner_visibility_sanitize',
-    ) );
-
-    $wp_customize->add_control('banner_visibility_control', array(
-            'description' => 'Display your site banner',
-            'label' => __('Banner Visibility', 'benjamin'),
-            'section' => 'header_settings_section',
-            'settings' => 'banner_visibility_setting',
-            'type' => 'select',
-            'choices' => array(
-                'hide' => 'Hide',
-                'display' => 'Display',
-            )
-        )
-    );
-
-
-    $wp_customize->add_setting('banner_text_setting', array(
-        'default' => '',
-        'sanitize_callback' => 'wp_filter_nohtml_kses',
-    ) );
-
-    $wp_customize->add_control('banner_text_control', array(
-            'label' => __('Banner Text', 'benjamin'),
-            'section' => 'header_settings_section',
-            'settings' => 'banner_text_setting',
-            'type' => 'text',
-        )
-    );
-
-
-    $wp_customize->add_setting('banner_read_more_setting', array(
-        'default' => '',
-        'sanitize_callback' => 'wp_filter_nohtml_kses',
-    ) );
-
-    $wp_customize->add_control('banner_read_more_control', array(
-            'label' => __('Banner Read More', 'benjamin'),
-            'section' => 'header_settings_section',
-            'settings' => 'banner_read_more_setting',
-            'type' => 'text',
-        )
-    );
-
+	$wp_customize->add_control(
+		'banner_read_more_control',
+		array(
+			'label'    => __( 'Banner Read More', 'benjamin' ),
+			'section'  => 'header_settings_section',
+			'settings' => 'banner_read_more_setting',
+			'type'     => 'text',
+		)
+	);
 
 }
-add_action('customize_register', 'benjamin_header_settings');
+
+add_action( 'customize_register', 'benjamin_header_settings' );
