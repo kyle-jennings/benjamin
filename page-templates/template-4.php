ff0b476d659d1b9c8d9a11f455ace772fb17230b
Steven Buccini <steven@stevenbuccini.com>
2019-08-18 14:52:51 -0700

    Add some responsiveness; add UI stuff

--- page-templates/template-4.php
+++ page-templates/template-4.php
@@ -32,17 +32,31 @@ if (!$hide_content) :
         endif;
         ?>
     <div class="main-content <?php echo esc_attr($main_width); ?>">
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+        <style>
+            .required {
+                color: red;
+            }
+        </style>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <script>
+            $(function() {
+                $(".datepicker").datepicker();
+            });
+        </script>
+
+
         <form>
+
             <div>
                 <h3>Business or Personal</h3>
                 <div>
-                    <p>Are you reporting on behalf of a business?</p>
-                    <input type="radio" name="business-report" id="business-report-yes" value="true">
+                    <p>Are you reporting on behalf of a business?<span class="required">*</span></p>
+                    <input type="radio" name="business-report" id="business-report-yes" value="true" required>
                     <label for="business-report-yes">Yes</label>
-                    <input type="radio" name="business-report" id="business-report-no" value="false">
+                    <input type="radio" name="business-report" id="business-report-no" value="false" required>
                     <label for="business-report-no">No</label>
                 </div>
             </div>
@@ -52,10 +66,10 @@ if (!$hide_content) :
                 <h3>Are you self-reporting, or reporting on behalf of someone else?</h3>
                 <div>
                     <p>Are you reporting an online incident, crime, scam, or a victimization of behalf of another person such as a
-                        parent, relative, or grandparent?</p>
-                    <input type="radio" name="behalf-of-others-yes" value="true">
+                        parent, relative, or grandparent?<span class="required">*</span></p>
+                    <input type="radio" name="behalf-of-others-yes" value="true" required>
                     <label for="behalf-of-others-yes">Yes</label>
-                    <input type="radio" name="behalf-of-others-no" value="false">
+                    <input type="radio" name="behalf-of-others-no" value="false" required>
                     <label for="behalf-of-others-no">No</label>
                 </div>
             </div>
@@ -65,25 +79,50 @@ if (!$hide_content) :
             <div>
                 <h3>Tell us what happened.</h3>
                 <div>
-                    <p>Please describe the incident in your own words.</p>
-                    <textarea></textarea>
+                    <p>Please describe the incident in your own words.<span class="required">*</span></p>
+                    <textarea required></textarea>
                 </div>
 
                 <div>
-                    <p>When did the incident occur?</p>
-                    <h4>TODO: jQuery UI datepicker</h4>
+                    <p>When did the incident occur?<span class="required">*</span></p>
+                    <input type="text" class="datepicker" required>
                 </div>
 
                 <div>
+                    <script>
+                        $(document).ready(function() {
+                            $('#ocurred-multiple-yes').change(function() {
+                                if (this.checked) {
+                                    $('#more-dates').append('<br><input type="text" class="datepicker" required>');
+                                    $('#more-dates').append('<br><input type="text" class="datepicker" required>');
+                                    $('#more-dates').append('<br><input type="text" class="datepicker" required>');
+                                }
+                            });
+                            $('#ocurred-multiple-no').change(function() {
+                                if (this.checked) {
+                                    $('#more-dates').empty();
+                                }
+                            });
+                        })
+                    </script>
                     <p>Did the incident occur multiple times. If yes, please list additional dates and times.</p>
-                    <h4>TODO: jQuery UI datepicker</h4>
-                    <h4>Also will need to figure out how this relation will be stored in the DB</h4>
+                    <label>Yes
+                        <input type="radio" id="ocurred-multiple-yes" name="occurred-multiple" value="true">
+                    </label>
+                    <label>No
+                        <input type="radio" id="ocurred-multiple-no" name="occurred-multiple" value="false" checked>
+                    </label>
+
+                    <input type="text" class="datepicker" required>
+
+                    <div id="more-dates"></div>
+
                 </div>
 
                 <div>
-                    <p>How would you categorize the incident?</p>
+                    <p>How would you categorize the incident?<span class="required">*</span></p>
                     <div>
-                        <input type="checkbox" name="financial-fraud" value="financial fraud">
+                        <input type="checkbox" name="financial-fraud" value="financial fraud" required>
                         <label for="financial-fraud">Financial fraud</label>
                     </div>
                     <div>
@@ -120,21 +159,50 @@ if (!$hide_content) :
             <hr>
 
             <div>
+                <script>
+                    $(document).ready(function() {
+                        $('#witnesses-yes').change(function() {
+                            if (this.checked) {
+                                $('#witnesses-names').show();
+                            }
+                        });
+                        $('#witnesses-no').change(function() {
+                            if (this.checked) {
+                                $('#witnesses-names').hide();
+                            }
+                        });
+                    })
+                </script>
                 <p>Are there witnesses or victims to this incident?</p>
                 <input type="radio" name="witnesses" id="witnesses-yes" value="true">
                 <label for="witnesses-yes">Yes</label>
-                <input type="radio" name="witnesses" id="witnesses-no" value="false">
+                <input type="radio" name="witnesses" id="witnesses-no" value="false" checked>
                 <label for="witnesses-no">No</label>
+
+                <div id="witnesses-names" style="display:none;">
+                    <p> Please provide their names.</p>
+                    <input type="text" id="witnesses-names">
+                </div>
             </div>
 
             <div>
+                <script>
+                    $(document).ready(function() {
+                        $('#fraud-vector').change(function() {
+                            // Eagerly hide because we don't know prior seleciton
+                            $('#extra-vector-info').children().hide();
+                            var selection = $('#fraud-vector').val();
+                            $('#picked-' + selection).show();
+                        })
+                    });
+                </script>
                 <h3>How did the incident occur?</h3>
                 <div>
-                    <p>How did the incident occur?</p>
-                    <select required>
+                    <p>How did the incident occur?<span class="required">*</span></p>
+                    <select id="fraud-vector" required>
                         <option value="">--Please choose an option--</option>
-                        <option value="home-phone">Home Phone</option>
-                        <option value="mobile-phone">Mobile-phone</option>
+                        <option value="home-phone">Home phone</option>
+                        <option value="mobile-phone">Mobile phone</option>
                         <option value="email">Email</option>
                         <option value="social-network">Social network</option>
                         <option value="messaging-app">Messaging app</option>
@@ -143,13 +211,64 @@ if (!$hide_content) :
                         <option value="voice-assisstant">Voice assistant</option>
                         <option value="other">Other</option>
                     </select>
+
+                    <div id="extra-vector-info">
+                        <div id="picked-home-phone" style="display:none">
+                            <p>Please enter your phone number and the alleged perpetrator's number, if available.</p>
+                            <label>Your phone number:
+                                <input type="tel">
+                            </label>
+                            <label>Their phone number:
+                                <input type="tel">
+                            </label>
+                        </div>
+                        <div id="picked-mobile-phone" style="display:none">
+                            <p>Please enter your phone number and the alleged perpetrator's number, if available.</p>
+                            <label>Your phone number:
+                                <input type="tel">
+                            </label>
+                            <label>Their phone number:
+                                <input type="tel">
+                            </label>
+                        </div>
+                        <div id="picked-email" style="display:none">
+                            <label>Your email:
+                                <input type="email" />
+                            </label>
+                            <label>Your email:
+                                <input type="email">
+                            </label>
+                        </div>
+                        <div id="picked-social-network" style="display:none">
+                            <label>Please select the social network:
+                                <select required>
+                                    <option value="">--Please choose an option--</option>
+                                    <option>Facebook</option>
+                                    <option>Twitter</option>
+                                    <option>Instagram</option>
+                                    <option>Pinterest</option>
+                                    <option>Ello</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </label>
+                            <p>Please enter your username and the alleged perpetrator's username, if available.</p>
+                            <label>Your user name:
+                                <input type="text">
+                            </label>
+                            <label>Their user name:
+                                <input type="text">
+                            </label>
+                        </div>
+                    </div>
                 </div>
 
-                <div>
-                    <p>Please enter (or copy+paste) the initial text that was used to contact you.</p>
-                    <textarea></textarea>
-                </div>
-                <!-- <div>
+            </div>
+
+            <div>
+                <p>Please enter (or copy+paste) the initial text that was used to contact you.</p>
+                <textarea></textarea>
+            </div>
+            <!-- <div>
         <p>Please enter your phone number and the alleged perpetrator's phone number, if available.</p>
         <label for="your-phone">Your phone number:</label>
         <input type="tel" name="your-phone">
@@ -157,18 +276,32 @@ if (!$hide_content) :
         <input type="tel" name="their-phone">
       </div> -->
 
-                <p>Did the incident involve a financial transaction such as transferring funds?</p>
-                <input type="radio" name="funds-transferred" id="funds-transferred-yes" value="true">
+            <div>
+                <p>Did the incident involve a financial transaction such as transferring funds?<span class="required">*</span>
+                </p>
+                <input type="radio" name="funds-transferred" id="funds-transferred-yes" value="true" required>
                 <label for="funds-transferred-yes">Yes</label>
-                <input type="radio" name="funds-transferred" id="funds-transferred-false" value="false">
+                <input type="radio" name="funds-transferred" id="funds-transferred-false" value="false" required>
                 <label for="funds-transferred-no">No</label>
             </div>
 
+
             <div>
-                <p>What is your username?</p>
-                <input type="text">
+                <p>Please enter your user name and the alleged perpetrator's user name, if available.</p>
+                <label for="your-username">Your username:</label>
+                <input name="your-username" type="text">
+                <label for="their-username">Their username:</label>
+                <input name="their-username" type="text">
             </div>
-        </form>
+
+
+    </div>
+
+    <div>
+        <p>What is your username?</p>
+        <input type="text">
+    </div>
+    </form>
 
 
     </div>
